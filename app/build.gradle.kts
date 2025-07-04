38c2f3dfae126f66298b024c5c39b60d96de8368
Biprangshu Das <dasbiprangshu@gmail.com>
2025-07-04 21:06:44 +0530

    added firebase ai studio

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,6 +57,7 @@ dependencies {
     implementation(libs.firebase.auth)
     implementation(libs.firebase.firestore)
     implementation(libs.firebase.storage)
+    implementation(libs.firebase.ai)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
